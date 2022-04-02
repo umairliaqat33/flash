278bf75077e7218a0e55babdc394dc2e7535be16
umair <umairliaqat552@gmail.com>
2022-04-02 22:00:10 +0500

    Splash removed

--- lib/main.dart
+++ lib/main.dart
@@ -12,34 +12,34 @@ class TorchApp extends StatefulWidget {
 }
 
 class _TorchAppState extends State<TorchApp> {
-  String img = "bulb off";
-  String txt = "Off";
-  Color th=Colors.black;
+  String img = "bulb off";     //Variable will be used to change images.
+  String txt = "Off";           //Variable used to change text of on/off
+  Color th=Colors.black;        //Variable to change color of background
   // ThemeData th=ThemeData.dark();
-  bool c=false;
+  bool c=false;                 //variable used to control conditional statements in many places.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: c?ThemeMode.light:ThemeMode.dark,
+      themeMode: c?ThemeMode.light:ThemeMode.dark,          //conditional statement used to change them according to on/off status.
       home: Scaffold(
         body: Container(
           color: th,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,            //will arrange image and text in center vertically.
+            mainAxisAlignment: MainAxisAlignment.center,              //will arrange image and text in center horizontally
             children: [
               TextButton(
                   style:ButtonStyle(
-                    splashFactory: NoSplash.splashFactory
+                    splashFactory: NoSplash.splashFactory             //removing onclick splash color
                   ),
                   onPressed: () {
                     setState(() {
                       if (img == "bulb on") {
-                        th=Colors.black;
+                        th=Colors.black;                              //changing color accordingly to on/off
                         img = "bulb off";
                         txt = "Off";
                         c=false;
-                        TorchLight.disableTorch();
+                        TorchLight.disableTorch();                    //used torch_Light package to control on or off and it is very simple just used on line to change flash light status but it is not recommended to use this way.
                       } else {
                         img = "bulb on";
                         txt = "On";
