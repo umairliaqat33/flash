e5ab6c1eccc9b1fddcb45336888dd087ad60709e
Umair <umairliaqat552@gmail.com>
2024-09-09 18:16:50 +0500

    re done the application

--- android/app/src/main/kotlin/com/example/flash/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flash/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.flash
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:torch_light/torch_light.dart';
 
 void main() {
-
-  runApp(TorchApp());
+  runApp(const TorchApp());
 }
 
 class TorchApp extends StatefulWidget {
+  const TorchApp({super.key});
+
   @override
   _TorchAppState createState() => _TorchAppState();
 }
 
 class _TorchAppState extends State<TorchApp> {
-  String img = "bulb off";     //Variable will be used to change images.
-  String txt = "Off";           //Variable used to change text of on/off
-  Color th=Colors.black;        //Variable to change color of background
+  String img = "bulb off"; //Variable will be used to change images.
+  String txt = "Off"; //Variable used to change text of on/off
+  Color th = Colors.black; //Variable to change color of background
   // ThemeData th=ThemeData.dark();
-  bool c=false;                 //variable used to control conditional statements in many places.
+  bool c =
+      false; //variable used to control conditional statements in many places.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: c?ThemeMode.light:ThemeMode.dark,          //conditional statement used to change them according to on/off status.
+      themeMode: c
+          ? ThemeMode.light
+          : ThemeMode
+              .dark, //conditional statement used to change them according to on/off status.
       home: Scaffold(
         body: Container(
           color: th,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,            //will arrange image and text in center vertically.
-            mainAxisAlignment: MainAxisAlignment.center,              //will arrange image and text in center horizontally
+            crossAxisAlignment: CrossAxisAlignment
+                .center, //will arrange image and text in center vertically.
+            mainAxisAlignment: MainAxisAlignment
+                .center, //will arrange image and text in center horizontally
             children: [
               TextButton(
-                  style:ButtonStyle(
-                    splashFactory: NoSplash.splashFactory             //removing onclick splash color
-                  ),
+                  style: const ButtonStyle(
+                      splashFactory:
+                          NoSplash.splashFactory //removing onclick splash color
+                      ),
                   onPressed: () {
                     setState(() {
                       if (img == "bulb on") {
-                        th=Colors.black;                              //changing color accordingly to on/off
+                        th =
+                            Colors.black; //changing color accordingly to on/off
                         img = "bulb off";
                         txt = "Off";
-                        c=false;
-                        TorchLight.disableTorch();                    //used torch_Light package to control on or off and it is very simple just used on line to change flash light status but it is not recommended to use this way.
+                        c = false;
+                        TorchLight
+                            .disableTorch(); //used torch_Light package to control on or off and it is very simple just used on line to change flash light status but it is not recommended to use this way.
                       } else {
                         img = "bulb on";
                         txt = "On";
-                        c=true;
-                        th=Colors.white;
+                        c = true;
+                        th = Colors.white;
                         TorchLight.enableTorch();
                       }
                     });
@@ -52,7 +62,10 @@ class _TorchAppState extends State<TorchApp> {
                   child: Image.asset("images/$img.png")),
               Text(
                 "Torch is $txt",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold,color: c?Colors.black:Colors.white),
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: c ? Colors.black : Colors.white),
               ),
             ],
           ),
